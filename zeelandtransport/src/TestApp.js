a51bec8f10366aa1f7a02a949fde6f60cd8f81c2
Remi <r.newton84@gmail.com>
2024-09-24 14:38:36 +0200

    TestFilterCMP

---
+++ zeelandtransport/src/TestApp.js
@@ -0,0 +1,5 @@
+function TestApp({ children }) {
+  return <div className="layout">Test</div>;
+}
+
+export default TestApp;
--- zeelandtransport/src/index.js
+++ zeelandtransport/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import TestApp from "./TestApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TestApp />
   </React.StrictMode>
 );
